ffdfc9182af62c5cc23512ec8e1db914c74867db
atulmalode <atulmalode@gmail.com>
2017-12-19 17:40:43 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -6,6 +6,7 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
+# Flask app should start in global layout
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
@@ -13,33 +14,38 @@ app = Flask(__name__)
 def webhook():
 	req = request.get_json(silent=True, force=True)
 	print(json.dumps(req, indent=4))
-	res = makeResponse(req)
+	res = processRequest(req)
 	res = json.dumps(res, indent=4)
+	# print(res)
 	r = make_response(res)
 	r.headers['Content-Type'] = 'application/json'
-	return r	
+	return r
 
 def makeResponse(req):
+	#if req.get("result").get("action") != "fetchWeatherForecast":
+	#	return {}
 	result = req.get("result")
 	parameters = result.get("parameters")
 	city = parameters.get("geo-city")
 	date = parameters.get("date")
-	r = request.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+',us&appid=b6907d289e10d714a6e88b30761fae22')
+	#if city is None:
+	#	return None
+	r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'us&appid=b6907d289e10d714a6e88b30761fae22')
 	json_object = r.json()
-	weather=json_object('list')
-	for i in Range(0,30):
+	weather=json_object['list']
+	for i in range(0,30):
 		if date in weather[i]['dt_txt']:
-			condition=weather[i]['weather'][0]['description']
+			condition= weather[i]['weather'][0]['description']
 			break
 
-	speech = "The forecast for " + city + " for " + date + " is " + condition
+	speech = "The forecast for"+city+ "for "+date+" is "+condition
 	return {
-		"speech": speech,
-		"displayText": speech,
-		"source": "apiai-weather-webhook"
-	}
-		
+    		"speech": speech,
+    		"displayText": speech,
+    		"source": "apiai-weather-webhook"
+    	}
+
 if __name__ == '__main__':
 	port = int(os.getenv('PORT', 5000))
-	print("Staring app on port %d" % port)
-	app.run(debug=False, port= port, host='0.0.0.0')
+	print("Starting app on port %d" % port)
+	app.run(debug=False, port=port, host='0.0.0.0')
